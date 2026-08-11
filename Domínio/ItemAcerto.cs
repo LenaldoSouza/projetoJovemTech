364193870c4cd69b530a631209623aed3b42025e
LenaldoSouza <souvinilenaldo@gmail.com>
2026-08-11 20:05:54 -0300

    init: inicialização do projeto, criação da primeira classe em C#, classe abstrata inicial do projeto.

---
+++ Domínio/ItemAcerto.cs
@@ -0,0 +1,17 @@
+using System.Windows.Markup;
+
+namespace Biblioteca.Domínio;
+
+public abstract class ItemAcervo
+{
+    public string? Titulo { get; set; } = string.Empty;
+    public string? Autor { get; set; } = string.Empty;
+    public bool Disponibilidade { get; set; } = true;
+    public abstract int PrazoDevolucao { get; set; }
+    public abstract decimal MultaDiaAtrasado { get; set; }
+
+    public decimal CalcularMulta(int diasAtrasados)
+    {
+        return diasAtrasados >= 0 ? diasAtrasados * MultaDiaAtrasado : diasAtrasados = 0;
+    }
+}
